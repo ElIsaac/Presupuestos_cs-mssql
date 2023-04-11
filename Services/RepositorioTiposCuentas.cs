5e9498e589d70d424bf5699c7fbba8905ee02223
elisaac <ramirezisaac.1518@gmail.com>
2023-04-10 22:02:08 -0600

    Agregar async await

--- Services/RepositorioTiposCuentas.cs
+++ Services/RepositorioTiposCuentas.cs
@@ -5,7 +5,7 @@ using Microsoft.Data.SqlClient;
 namespace ManejoPresupuesto.Services
 {
     public interface IRepositorioTiposCuentas{
-        void Crear(TipoCuenta tipoCuenta);
+        Task Crear(TipoCuenta tipoCuenta);
     }
     public class RepositorioTiposCuentas:IRepositorioTiposCuentas{
         private readonly string connectionString;
@@ -13,9 +13,9 @@ namespace ManejoPresupuesto.Services
         {
             connectionString = configuration.GetConnectionString("DefaultConnection");
         }
-        public void Crear(TipoCuenta tipoCuenta){
+        public async Task Crear(TipoCuenta tipoCuenta){
             using var connection = new SqlConnection(connectionString);
-            var id = connection.QuerySingle<int>($@"insert into TiposCuentas (Nombre, UsuarioId, Orden) values (@Nombre, @UsuarioId, 0);
+            var id = await connection.QuerySingle<int>($@"insert into TiposCuentas (Nombre, UsuarioId, Orden) values (@Nombre, @UsuarioId, 0);
                                                     Select SCOPE_IDENTITY();", tipoCuenta);
             tipoCuenta.Id=id;
         }
